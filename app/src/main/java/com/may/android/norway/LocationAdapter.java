52614d1a15d9c2028d9f2d181d7b19adbdfe420b
khmmay <khm.may@gmail.com>
2017-05-31 00:52:45 +0200

    After review

--- app/src/main/java/com/may/android/norway/LocationAdapter.java
+++ app/src/main/java/com/may/android/norway/LocationAdapter.java
@@ -38,24 +38,24 @@ public class ElemAdapter extends ArrayAdapter<ContentElement> {
 
 
         // Get the {@link AndroidFlavor} object located at this position in the list
-        final ContentElement currentWord = getItem(position);
+        final ContentElement currentElem = getItem(position);
 
         // Find the TextView in the list_item.xml layout with the ID version_name
         TextView miTextView = (TextView) listItemView.findViewById(R.id.heading);
         // Get the version name from the current AndroidFlavor object and
         // set this text on the name TextView
-        miTextView.setText(currentWord.getName());
+        miTextView.setText(currentElem.getName());
 
         // Find the TextView in the list_item.xml layout with the ID version_number
         TextView enTextView = (TextView) listItemView.findViewById(R.id.description);
         // Get the version number from the current AndroidFlavor object and
         // set this text on the number TextView
-        enTextView.setText(currentWord.getDescription());
+        enTextView.setText(currentElem.getDescription());
 
         TextView navigateView = (TextView) listItemView.findViewById(R.id.navigate);
         ImageView imageView = (ImageView) listItemView.findViewById(R.id.elemImg);
-        if (currentWord.getHasImage()) {
-            imageView.setImageResource(currentWord.getImageID());
+        if (currentElem.getHasImage()) {
+            imageView.setImageResource(currentElem.getImageID());
         } else {
             navigateView.setVisibility(View.GONE);
             imageView.setVisibility(View.GONE);
@@ -64,7 +64,7 @@ public class ElemAdapter extends ArrayAdapter<ContentElement> {
         navigateView.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
-                Uri gmmIntentUri = Uri.parse(currentWord.getGeoLocation());
+                Uri gmmIntentUri = Uri.parse(currentElem.getGeoLocation());
                 Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
                 mapIntent.setPackage("com.google.android.apps.maps");
                 getContext().startActivity(mapIntent);
